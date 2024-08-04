7002be7ef1ed352e36430de3574fa33597c523e6
JillyMan <artyom543211@gmail.com>
2024-08-04 14:25:31 +0200

    Add workout validation.

---
+++ src/components/exercises.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Form, Button, message } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { Excersise } from './exercise';
+
+export const Exercises = () => {
+    return (
+        <Form.List
+            name="exercises"
+            rules={[
+                {
+                    validator: async (_, exercises) => {
+                        if (!exercises || exercises.length < 1) {
+                            message.error("At least one exercises are required!")
+                            return Promise.reject(new Error('At least two exercises are required'));
+                        }
+                        return Promise.resolve();
+                    },
+                },
+            ]}
+        >
+            {(fields, { add, remove }) => (
+                <>
+                    <Excersise
+                        fields={fields}
+                        onRemove={(key) => remove(key)} />
+                    <Form.Item>
+                        <Button
+                            style={{ display: 'block', margin: '16px auto' }}
+                            type="dashed"
+                            onClick={() => add()}
+                            icon={<PlusOutlined />}>
+                            Add Exercise
+                        </Button>
+                    </Form.Item>
+                </>
+            )}
+        </Form.List>
+    );
+};
