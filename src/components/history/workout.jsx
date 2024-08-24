6292c4888d54f63cf71b5c1cc007e90f73721aae
JillyMan <artyom543211@gmail.com>
2024-08-24 19:34:40 +0200

    Move out of components.

--- src/components/history/workout.jsx
+++ src/components/history/workout.jsx
@@ -3,20 +3,20 @@ import { ExercisesView } from "./exercises-view";
 import { ClockCircleOutline } from "antd-mobile-icons";
 import "./style.css";
 
+function calculateDuration(startTime, finishTime) {
+	const timeDifference = new Date(finishTime - startTime);
+	const hours = Math.floor(timeDifference / 3600000);
+	const minutes = Math.round((timeDifference % 3600000) / 60000);
+
+	return `${hours <= 1 ? "" : hours + "h"} ${minutes}m`;
+}
+
 const WorkoutDuration = ({ startTime, finishTime }) => {
-	const calculateDuration = () => {
-		const timeDifference = new Date(finishTime - startTime);
-		const hours = Math.floor(timeDifference / 3600000);
-		const minutes = Math.round((timeDifference % 3600000) / 60000);
-
-		return `${hours < 1 ? "" : hours + "h"} ${minutes}m`;
-	};
-
 	return (
 		<AutoCenter>
 			<Tag round="true" className="workout-duration">
 				<ClockCircleOutline />
-				<span>{calculateDuration()}</span>
+				<span>{calculateDuration(startTime, finishTime)}</span>
 			</Tag>
 		</AutoCenter>
 	);
--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 function calculateTimePassed(startTime: Date) {
 	const start = new Date(startTime).getTime();
@@ -6,8 +6,21 @@ function calculateTimePassed(startTime: Date) {
 	return now - start;
 }
 
-export function Timer({startTime}: { startTime: Date }) {
-	const [timePassed, setTimePassed] = useState(calculateTimePassed(startTime));
+function formatTime(milliseconds: number) {
+	const totalSeconds = Math.floor(milliseconds / 1000);
+	const hours = Math.floor(totalSeconds / 3600);
+	const minutes = Math.floor((totalSeconds % 3600) / 60);
+	const seconds = totalSeconds % 60;
+
+	return `${hours.toString().padStart(2, "0")}:${minutes
+		.toString()
+		.padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+}
+
+export function Timer({ startTime }: { startTime: Date }) {
+	const [timePassed, setTimePassed] = useState(
+		calculateTimePassed(startTime)
+	);
 
 	useEffect(() => {
 		const intervalId = setInterval(() => {
@@ -17,16 +30,5 @@ export function Timer({startTime}: { startTime: Date }) {
 		return () => clearInterval(intervalId);
 	}, [startTime]);
 
-	const formatTime = (milliseconds: number) => {
-		const totalSeconds = Math.floor(milliseconds / 1000);
-		const hours = Math.floor(totalSeconds / 3600);
-		const minutes = Math.floor((totalSeconds % 3600) / 60);
-		const seconds = totalSeconds % 60;
-
-		return `${hours.toString().padStart(2, "0")}:${minutes
-			.toString()
-			.padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-	};
-
-	return <p style={{fontSize: "32px"}}>{formatTime(timePassed)}</p>;
+	return <p style={{ fontSize: "32px" }}>{formatTime(timePassed)}</p>;
 }
