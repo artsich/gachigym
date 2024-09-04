df959ce0ca470ea92be17bb48071a9123bbaded8
JillyMan <artyom543211@gmail.com>
2024-09-04 19:11:41 +0200

    Reverse activities.

--- src/components/workout-activity-panel/index.tsx
+++ src/components/workout-activity-panel/index.tsx
@@ -14,7 +14,18 @@ function formatDate(date: Date) {
 	return `${year}-${month}-${day}`;
 }
 
-function groupByDateCount(workouts: any) {
+const today = new Date();
+const fourMonthsAgo = new Date(
+	today.getFullYear(),
+	today.getMonth() - 4,
+	today.getDate()
+);
+
+interface DateCountMap {
+	[date: string]: number;
+}
+
+function groupByDateCount(workouts: any): DateCountMap {
 	return workouts.reduce((acc: any, workout: any) => {
 		const workoutDate = formatDate(new Date(workout.finishTime));
 		if (!acc[workoutDate]) {
@@ -25,51 +36,46 @@ function groupByDateCount(workouts: any) {
 	}, {});
 }
 
+function workoutToActivity(workouts: any): Activity[] {
+	const workoutMap = groupByDateCount(workouts);
+
+	if (!(formatDate(today) in workoutMap)) {
+		workoutMap[formatDate(today)] = 0;
+	}
+
+	if (!(formatDate(fourMonthsAgo) in workoutMap)) {
+		workoutMap[formatDate(fourMonthsAgo)] = 0;
+	}
+
+	return Object.entries(workoutMap)
+		.map(([date, count]) => ({
+			date,
+			count: count,
+			level: count >= 4 ? 4 : count,
+		}))
+		.sort((a, b) => compareDates(a.date, b.date));
+}
+
+function compareDates(date1: string, date2: string): number {
+	return new Date(date1).getTime() - new Date(date2).getTime();
+}
+
 const githubThemeColors: ThemeInput = {
 	dark: ["#161b22", "#0e4429", "#006d32", "#26a641", "#39d353"],
 	light: ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"],
 };
 
 export const WorkoutActivityPanel = () => {
-	const today = new Date();
-	const fourMonthsAgo = new Date(
-		today.getFullYear(),
-		today.getMonth() - 4,
-		today.getDate()
-	);
-
 	const { theme } = useTheme();
+	// TODO: when workout create, this component does not update state :(
 	const [workouts] = useState(() => getWorkoutsInRange(fourMonthsAgo, today));
-
-	const workoutMap = groupByDateCount(workouts);
-
-	const values: Activity[] = [
-		// today and fourMonthsAgo is required
-		// to display panel when workouts.lenght === 0
-		{
-			date: formatDate(today),
-			count: 0,
-			level: 0,
-		},
-		...Object.entries(workoutMap).map(([date, count]) => ({
-			date,
-			count: count as number,
-			level: (count as number) > 2 ? 4 : 1,
-		})),
-		{
-			date: formatDate(fourMonthsAgo),
-			count: 0,
-			level: 0,
-		},
-	];
 
 	return (
 		<>
 			<ActivityCalendar
 				colorScheme={theme}
 				theme={githubThemeColors}
-				data={values}
-				loading={values.length === 0}
+				data={workoutToActivity(workouts)}
 				renderBlock={(block, activity) =>
 					React.cloneElement(block, {
 						"data-tooltip-id": "react-tooltip",
