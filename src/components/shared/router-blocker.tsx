e84036d237c11bc28a7d7869e3514c6c43371ad1
Artsiom Dubinevich <34747326+artsich@users.noreply.github.com>
2024-09-09 14:03:27 +0200

    Add router blocker. (#37)

---
+++ src/components/shared/router-blocker.tsx
@@ -0,0 +1,56 @@
+import { Modal } from "antd-mobile";
+import { useEffect } from "react";
+import { useBlocker } from "react-router-dom";
+
+const useNavigationBlocker = (when: boolean) => {
+	useEffect(() => {
+		const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+			if (when) {
+				const message =
+					"You have unsaved changes. Are you sure you want to leave?";
+				event.preventDefault();
+				return message;
+			}
+		};
+
+		window.addEventListener("beforeunload", handleBeforeUnload);
+
+		return () => {
+			window.removeEventListener("beforeunload", handleBeforeUnload);
+		};
+	}, [when]);
+};
+
+export const RouterBlocker = ({ title }: { title: string }) => {
+	let blocker = useBlocker(
+		({ currentLocation, nextLocation }) =>
+			currentLocation.pathname !== nextLocation.pathname
+	);
+
+	useNavigationBlocker(true);
+
+	return (
+		<Modal
+			visible={blocker.state === "blocked"}
+			content={title}
+			closeOnAction
+			onAction={(a) => {
+				if (a.key === "leave" && blocker.proceed) {
+					blocker.proceed();
+				} else if (a.key === "stay" && blocker.reset) {
+					blocker.reset();
+				}
+			}}
+			actions={[
+				{
+					key: "leave",
+					text: "Leave",
+				},
+				{
+					key: "stay",
+					text: "Stay",
+				},
+			]}
+		/>
+	);
+};
