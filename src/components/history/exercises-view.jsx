500f463918247f7fe42b3df39927515dd35ba759
Artemeow <artem.zhulpa@mail.ru>
2024-09-10 19:16:48 +0300

    added information about training tonnage

--- src/components/history/exercises-view.jsx
+++ src/components/history/exercises-view.jsx
@@ -1,6 +1,25 @@
 import { Grid, Space, Tag } from "antd-mobile";
 import { useState } from "react";
 import { SetBadge } from "./set-badge";
+import { InformationCircleFill } from "antd-mobile-icons";
+import { ThemedPopover } from "../shared/themed-popover";
+
+const getExerciseInformation = (sets) => {
+	const tonnage = sets.length
+		? sets?.reduce(
+				(tonnage, set) => tonnage + (set.weight || 0) * (set.reps || 0),
+				0
+		  )
+		: 0;
+
+	const reps = sets.length
+		? sets?.reduce((summaryReps, set) => {
+				return summaryReps + set.reps;
+		  }, 0)
+		: 0;
+
+	return `Tonnage: ${tonnage} kg for ${reps} reps`;
+};
 
 export const ExercisesView = ({ exercises }) => {
 	const [activeExercise, setActiveExercise] = useState(0);
@@ -27,19 +46,38 @@ export const ExercisesView = ({ exercises }) => {
 				</Space>
 			</Grid.Item>
 			<Grid.Item>
-				<Grid columns={6} gap={16}>
-					{exercises.length ? (
-						exercises[activeExercise].sets?.map((set, ind2) => (
-							<Grid.Item key={ind2}>
-								<SetBadge
-									weight={set.weight ?? 0}
-									reps={set.reps ?? 0}
-								/>
-							</Grid.Item>
-						))
-					) : (
-						<div></div>
-					)}
+				<Grid columns={7} gap={4}>
+					<Grid.Item span={6}>
+						<Grid columns={5} gap={16}>
+							{exercises.length ? (
+								exercises[activeExercise].sets?.map(
+									(set, ind2) => (
+										<Grid.Item key={ind2}>
+											<SetBadge
+												weight={set.weight ?? 0}
+												reps={set.reps ?? 0}
+											/>
+										</Grid.Item>
+									)
+								)
+							) : (
+								<div></div>
+							)}
+						</Grid>
+					</Grid.Item>
+					<Grid.Item span={1}>
+						<AutoCenter>
+							<ThemedPopover
+								content={getExerciseInformation(
+									exercises[activeExercise].sets
+								)}
+								trigger="click"
+								placement="bottom"
+							>
+								<InformationCircleFill fontSize={20} />
+							</ThemedPopover>
+						</AutoCenter>
+					</Grid.Item>
 				</Grid>
 			</Grid.Item>
 		</Grid>
