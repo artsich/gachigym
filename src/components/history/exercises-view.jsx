641497bfb8b97e1248b77823c63fa0ebeb27347f
Artemeow <43747062+Artemeow@users.noreply.github.com>
2024-09-11 11:47:33 +0300

    added information about training tonnage (#39)
    * added information about training tonnage
    ---------
    
    Co-authored-by: JillyMan <artyom543211@gmail.com>

--- src/components/history/exercises-view.jsx
+++ src/components/history/exercises-view.jsx
@@ -1,6 +1,48 @@
-import { Grid, Space, Tag } from "antd-mobile";
+import { AutoCenter, Grid, Space, Tag } from "antd-mobile";
 import { useState } from "react";
 import { SetBadge } from "./set-badge";
+import { InformationCircleFill } from "antd-mobile-icons";
+import { ThemedPopover } from "../shared/themed-popover";
+
+function getSetsInformation(sets) {
+	const tonnage =
+		sets?.reduce(
+			(tonnage, set) =>
+				tonnage + Number(set.weight || 0) * Number(set.reps || 0),
+			0
+		) ?? 0;
+
+	const reps =
+		sets?.reduce(
+			(summaryReps, set) => summaryReps + Number(set.reps || 0),
+			0
+		) ?? 0;
+
+	if (tonnage === 0) {
+		return `${reps} reps`;
+	} else {
+		return `Tonnage: ${tonnage} kg for ${reps} reps`;
+	}
+}
+
+const ExerciseSummary = ({ exercise }) => {
+	const sets = exercise?.sets;
+	if (!sets) {
+		return <></>;
+	}
+
+	return (
+		<AutoCenter>
+			<ThemedPopover
+				content={getSetsInformation(sets)}
+				trigger="click"
+				placement="bottom"
+			>
+				<InformationCircleFill fontSize={20} />
+			</ThemedPopover>
+		</AutoCenter>
+	);
+};
 
 export const ExercisesView = ({ exercises }) => {
 	const [activeExercise, setActiveExercise] = useState(0);
@@ -27,19 +69,28 @@ export const ExercisesView = ({ exercises }) => {
 				</Space>
 			</Grid.Item>
 			<Grid.Item>
-				<Grid columns={6} gap={16}>
-					{exercises.length ? (
-						exercises[activeExercise].sets?.map((set, ind2) => (
-							<Grid.Item key={ind2}>
-								<SetBadge
-									weight={set.weight ?? 0}
-									reps={set.reps ?? 0}
-								/>
-							</Grid.Item>
-						))
-					) : (
-						<div></div>
-					)}
+				<Grid columns={7} gap={4}>
+					<Grid.Item span={6}>
+						<Grid columns={5} gap={16}>
+							{exercises.length ? (
+								exercises[activeExercise].sets?.map(
+									(set, ind2) => (
+										<Grid.Item key={ind2}>
+											<SetBadge
+												weight={set.weight ?? 0}
+												reps={set.reps ?? 0}
+											/>
+										</Grid.Item>
+									)
+								)
+							) : (
+								<div></div>
+							)}
+						</Grid>
+					</Grid.Item>
+					<Grid.Item span={1}>
+						<ExerciseSummary exercise={exercises[activeExercise]} />
+					</Grid.Item>
 				</Grid>
 			</Grid.Item>
 		</Grid>
